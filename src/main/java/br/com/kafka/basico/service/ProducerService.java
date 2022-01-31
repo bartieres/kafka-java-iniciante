c3e3152541c1de20d1c63b9ef5595f29b685ea37
bartieres <andre.bartires@gmail.com>
2022-01-31 13:03:57 -0300

    Adiciona key para producer e client id para consumer

--- src/main/java/br/com/kafka/basico/service/ProducerService.java
+++ src/main/java/br/com/kafka/basico/service/ProducerService.java
@@ -6,14 +6,16 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class ProducerService {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "3423434,546456,4321";
-        var record = new ProducerRecord<>("TOPIC_TESTE", value, value);
+        var key = UUID.randomUUID().toString();
+        var value = key + "546456,4321";
+        var record = new ProducerRecord<>("TOPIC_TESTE", key, value);
         producer.send(record, (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
